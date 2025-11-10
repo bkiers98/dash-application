206b43d4926c354f8261a8d9ff465696d15ba42b
Ben Kiers <benkiers@macbookair.lan>
2025-11-10 17:00:50 -0500

    Debug for Render III

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import pandas as pd
 
 
-app = Dash()
+app = Dash("Video Game Sales")
 server=app.server
 
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSNyYIn22Bai_3cR_oKjvFodaJw5uEESzcEjO9ZfwMjIHtEWVNts4rzPwsxug8-qLe5JYmRKdCEfcP3/pub?output=csv')
@@ -13,12 +13,13 @@ df_sr = df_sales_region[df_sales_region['genre'] == 'Action']
 df_genre = df[df['genre'] == 'Platform']
 df_grouped = df.groupby('genre')[['total_sales', 'na_sales', 'jp_sales', 'pal_sales', 'other_sales']].mean()
 
-app = Dash("Video Game Sales")
-
 fig_pie = px.pie(df_sr, values='sales', names='sales_region', title='Regional Sales by Genre')
 fig_scatter = px.scatter(df_genre, x='na_sales', y='critic_score', color='release_year', hover_data=['title', 'release_year'], title='Sales vs Critic Scores')
 
 fig_dot = px.scatter(title="Average Sales in Each Genre")
+fig_dot.add_trace(px.scatter(df_grouped, x=df_grouped.index, y='total_sales', color_discrete_sequence=['red']).data[0])
+fig_dot.add_trace(px.scatter(df_grouped, x=df_grouped.index, y='na_sales', color_discrete_sequence=['blue']).data[0])
+fig_dot.update_traces(marker=dict(size=10))
 
 app.layout = html.Div(children=[
     html.H1(children='Video Game Sales Dashboard',
@@ -38,9 +39,7 @@ app.layout = html.Div(children=[
 
 # add functions here
 
-fig_dot.add_trace(px.scatter(df_grouped, x=df_grouped.index, y='total_sales', color_discrete_sequence=['red']).data[0])
-fig_dot.add_trace(px.scatter(df_grouped, x=df_grouped.index, y='na_sales', color_discrete_sequence=['blue']).data[0])
-fig_dot.update_traces(marker=dict(size=10))
+
 
 # DO NOT EDIT BELOW THIS LINE (except to change `jupyter_height`)
 
